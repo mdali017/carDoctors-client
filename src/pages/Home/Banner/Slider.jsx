e49a24b49c99fe50b60bd423ade25a1ea9535292
MD MOHABBAT ALI <mohabbatalit8@gmail.com>
2023-07-29 10:14:35 +0600

    Create Header, Footer, Navber

---
+++ src/pages/Home/Banner/Slider.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Slider = ({ img }) => {
+  return (
+    <div>
+      <div className="bg-gray-900 ">
+        <img src={img} className="w-full opacity-50" />
+      </div>
+      <div className="absolute flex  transform -translate-y-1/2 left-5 right-5 bottom-[100px] ">
+        <div className="text-white w-1/2">
+          <h2 className="text-6xl font-bold mb-4">
+            Affordable Price For Car Servicing
+          </h2>
+          <p className="mb-5">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+            eius enim hic saepe voluptatum soluta, quia dolorem amet laboriosam
+            recusandae tenetur dolor fugit deleniti possimus!
+          </p>
+          <div>
+            <button className="btn btn-primary mr-6">Discover More</button>
+            <button className="btn btn-outline btn-warning">
+              Latest Project
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
---
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../../assets/icons/logo.svg";
+
+const NavBar = () => {
+  const [bg, setBG] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      return window.scrollY > 50 ? setBG(true) : setBG(false);
+    });
+  }, []);
+  const navItems = (
+    <>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link>About</Link>
+      </li>
+    </>
+  );
+
+  return (
+    // <div className={`${bg ? "bg-gray-600 text-white h-24" : "h-20"}`}>
+    <div>
+      {/* <div className="navbar bg-base-100 h-28 fixed z-10 max-w-7xl"> */}
+      <div
+        className={`${
+          bg ? "bg-slate-800 h-24 p-5" : "bg-slate-400 h-24"
+        } navbar max-w-7xl flex items-center fixed top-0 w-full text-white z-10 transition-all duration-300`}
+      >
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {navItems}
+            </ul>
+          </div>
+          <Link to="/" className=" text-xl">
+            <img src={logo} alt="" />
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{navItems}</ul>
+        </div>
+        <div className="navbar-end">
+          <button className="btn btn-outline btn-warning">Appointment</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
